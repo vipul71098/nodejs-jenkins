cc8f5bbf4483f4aea4394629fb718ce38d0c248a
vipul71098 <38537174+vipul71098@users.noreply.github.com>
2022-08-23 14:38:39 +0530

    Create nodejsdocker.groovy

---
+++ job-dsl/nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/vipul71098/nodejs-jenkins.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('vipul71098')
+            node / gitConfigEmail('vipul71098@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('vipulshukla71098/node-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('	docker-hub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
